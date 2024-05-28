050f2a0e51b1e1788735bf9626f44c403c0aebe2
gerardo talavera <gerardotalvera@gmail.com>
2024-05-27 19:35:28 -0700

    mobile swap functionality

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.querySelector('#app').innerHTML = `
 
      <div class="swiper-slide">
      <img class="food" src="https://apheiro.github.io/restaurant_page/93420905b048d48e4c74.png">
-    
+    <div class="information">
         <h2>Hot - Dog</h2>
         <hr>
         <div class="card">
@@ -25,12 +25,13 @@ document.querySelector('#app').innerHTML = `
      ever since the 1500s, when an unknown printer took a galley of type and scrambled 
      it to make a type specimen book.
    </p>
+   </div>
       </div>
       </div>
 
       <div class="swiper-slide">
       <img class="food" src="https://apheiro.github.io/restaurant_page/ae3c0fb9558fca9ffeea.png">
-     
+      <div class="information">
          <h2>Hamburger</h2>
          <hr>
          <div class="card">
@@ -39,12 +40,13 @@ document.querySelector('#app').innerHTML = `
       ever since the 1500s, when an unknown printer took a galley of type and scrambled 
       it to make a type specimen book.
     </p>
+    </div>
        </div>
        </div>
 
        <div class="swiper-slide">
        <img class="food" src="https://apheiro.github.io/restaurant_page/f231df94bf646111906f.png">
-      
+       <div class="information">
           <h2>French Fries</h2>
           <hr>
           <div class="card">
@@ -53,14 +55,15 @@ document.querySelector('#app').innerHTML = `
        ever since the 1500s, when an unknown printer took a galley of type and scrambled 
        it to make a type specimen book.
      </p>
+     </div>
         </div>
         </div>
 
         <div class="swiper-slide">
         <br>
         <br>
-    <img class="food" src="https://apheiro.github.io/restaurant_page/41419795e2a2ad3f83b6.png">
-   
+    <img class="food pizza" src="https://apheiro.github.io/restaurant_page/41419795e2a2ad3f83b6.png">
+   <div class="information">
        <h2>Pizza</h2>
        <hr>
        <div class="card">
@@ -69,6 +72,7 @@ document.querySelector('#app').innerHTML = `
     ever since the 1500s, when an unknown printer took a galley of type and scrambled 
     it to make a type specimen book.
   </p>
+  </div>
      </div>
      </div>
 
@@ -77,8 +81,8 @@ document.querySelector('#app').innerHTML = `
 
    <div class="swiper-pagination"></div>
 
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
+  <div class="swiper-button-prev btn"></div>
+  <div class="swiper-button-next btn"></div>
 
   <div class="swiper-scrollbar"></div>
 
