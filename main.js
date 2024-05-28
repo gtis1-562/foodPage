877ff39690bab191dd7fe6e8e2b6a0f63652001e
gerardo talavera <gerardotalvera@gmail.com>
2024-05-27 18:12:17 -0700

    added 3d swipping

--- main.js
+++ main.js
@@ -1,24 +1,111 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import logo from './food-logo.png'
+
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+<div id="header">
+
+<a  target="_blank">
+      <img src="${logo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
+</div>
+
+
+<div class="swiper">
+<div class="swiper-wrapper">
+
+     <div class="swiper-slide">
+     <img class="food" src="https://apheiro.github.io/restaurant_page/93420905b048d48e4c74.png">
+    
+        <h2>Hot - Dog</h2>
+        <hr>
+        <div class="card">
+   <p class="food-info">Lorem Ipsum is simply dummy text of the printing and
+    typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+     ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+     it to make a type specimen book.
+   </p>
+      </div>
+      </div>
+
+      <div class="swiper-slide">
+      <img class="food" src="https://apheiro.github.io/restaurant_page/ae3c0fb9558fca9ffeea.png">
+     
+         <h2>Hamburger</h2>
+         <hr>
+         <div class="card">
+    <p class="food-info">Lorem Ipsum is simply dummy text of the printing and
+     typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+      ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+      it to make a type specimen book.
     </p>
-  </div>
+       </div>
+       </div>
+
+       <div class="swiper-slide">
+       <img class="food" src="https://apheiro.github.io/restaurant_page/f231df94bf646111906f.png">
+      
+          <h2>French Fries</h2>
+          <hr>
+          <div class="card">
+     <p class="food-info">Lorem Ipsum is simply dummy text of the printing and
+      typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+       ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+       it to make a type specimen book.
+     </p>
+        </div>
+        </div>
+
+        <div class="swiper-slide">
+    <img class="food" src="https://apheiro.github.io/restaurant_page/41419795e2a2ad3f83b6.png">
+   
+       <h2>Pizza</h2>
+       <hr>
+       <div class="card">
+  <p class="food-info">Lorem Ipsum is simply dummy text of the printing and
+   typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+    ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+    it to make a type specimen book.
+  </p>
+     </div>
+     </div>
+
+<!-- end -->
+      </div>                    <!-- swipper-swrapper -->
+
+   <div class="swiper-pagination"></div>
+
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <div class="swiper-scrollbar"></div>
+
+ </div>
+
+
+
 `
 
-setupCounter(document.querySelector('#counter'))
+let mySwipper = new Swiper('.swiper', {
+  effect: 'cube',
+    grabCursor:true,
+    cubeEffect: {
+      shadow:true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
